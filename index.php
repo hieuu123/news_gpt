7d47b50237f77fa6a52361cf2029ea8a489015e4
hieuu123 <tronghieuabc123zzz@gmail.com>
2023-10-12 16:45:00 +0700

    test

--- index.php
+++ index.php
@@ -44,7 +44,6 @@
     <!-- Preloader Start -->
     <header>
         <!-- Header Start -->
-        <h1>Hello</h1>
        <div class="header-area">
             <div class="main-header">
                 <div class="header-top black-bg d-none d-md-block">
