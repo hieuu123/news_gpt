fa924a21d78662cebb0e21f70187ba72b198d5af
hieuu123 <140571471+hieuu123@users.noreply.github.com>
2023-11-30 18:51:13 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,278 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>News HTML-5 Template </title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/ticker-style.css">
+    <link rel="stylesheet" href="assets/css/flaticon.css">
+    <link rel="stylesheet" href="assets/css/slicknav.css">
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/css/themify-icons.css">
+    <link rel="stylesheet" href="assets/css/slick.css">
+    <link rel="stylesheet" href="assets/css/nice-select.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/hqn.css">
+
+    <!-- Fontawesome -->
+    <script src="https://kit.fontawesome.com/d3b4b6d594.js" crossorigin="anonymous"></script>
+
+    <style>
+        #footer-nav {
+            margin-top: 150px;
+        }
+
+        #footer-nav .sub-menu-category a {
+            color: #002e5b;
+            font-weight: 500;
+        }
+
+        #footer-nav .sub-menu-category a:hover,
+        #footer-nav .text-right a:hover {
+            color: #FC3F00;
+        }
+
+        .footer-bottom {
+            background-color: #100f0f;
+            color: white;
+            margin: 0;
+            height: 35px;
+        }
+
+        .copyright-text {
+            line-height: 35px;
+        }
+    </style>
+</head>
+
+<body>
+    <footer>
+        <!-- Footer Start-->
+        <div class="footer-area footer-padding fix">
+            <div class="container">
+                <div class="row d-flex justify-content-between">
+                    <div class="col-xl-4 col-lg-5 col-md-7 col-sm-12"> <!-- col-xl-5 -->
+                        <div class="single-footer-caption">
+                            <div class="single-footer-caption">
+                                <!-- logo -->
+                                <div class="footer-logo">
+                                    <a href="index.php"><img src="assets/img/logo/hqn_logo/logo_slogan_footer.jpg" alt="" width="70%"></a>
+                                </div>
+                                <div class="footer-tittle">
+                                    <div class="footer-pera">
+                                        <p>Chào mừng bạn đến với QHA - Trang tin tức hàng đầu với cam kết "Nhanh - Mới - Chính xác"! Chúng tôi tự hào là nguồn cung cấp thông tin hàng đầu, mang đến cho độc giả những tin tức mới nhất và chính xác nhất từ mọi lĩnh vực.</p>
+                                    </div>
+                                </div>
+                                <!-- social -->
+                                <div class="footer-social">
+                                    <a href="#"><i class="fab fa-twitter"></i></a>
+                                    <a href="#"><i class="fab fa-instagram"></i></a>
+                                    <a href="#"><i class="fab fa-pinterest-p"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-7" id="footer-nav">
+                        <div class="row">
+                            <?php
+                            require_once "connectSql.php";
+                            $sql = "SELECT categories.*
+                                From categories where category_id <= '5'";
+                            $result = $conn->query($sql);
+                            while (($row = $result->fetch_assoc())) {
+                                echo "
+                                            <span class='col sub-menu-category'><a href='content_cate.php?category_id={$row['category_id']}'>
+                                            {$row['category_name']}</a></span>
+                                        ";
+                            }
+                            ?>
+                        </div>
+                        <div class="row">
+                            <?php
+                            require_once "connectSql.php";
+                            $sql = "SELECT categories.*
+                                From categories where category_id <= '10' and category_id >'5'";
+                            $result = $conn->query($sql);
+                            while (($row = $result->fetch_assoc())) {
+                                echo "
+                                            <span class='col sub-menu-category'><a href='content_cate.php?category_id={$row['category_id']}'>
+                                            {$row['category_name']}</a></span>
+                                        ";
+                            }
+                            ?>
+                        </div>
+                        <div class="row">
+                            <?php
+                            require_once "connectSql.php";
+                            $sql = "SELECT categories.*
+                                From categories where category_id <= '15' and category_id > '10'";
+                            $result = $conn->query($sql);
+                            while (($row = $result->fetch_assoc())) {
+                                echo "
+                                            <span class='col sub-menu-category'><a href='content_cate.php?category_id={$row['category_id']}'>
+                                            {$row['category_name']}</a></span>
+                                        ";
+                            }
+                            ?>
+                        </div>
+                        <div class="row">
+                            <?php
+                            require_once "connectSql.php";
+                            $sql = "SELECT categories.*
+                                From categories where category_id <= '20' and category_id>'15'";
+                            $result = $conn->query($sql);
+                            while (($row = $result->fetch_assoc())) {
+                                echo "
+                                            <span class='col sub-menu-category'><a href='content_cate.php?category_id={$row['category_id']}'>
+                                            {$row['category_name']}</a></span>
+                                        ";
+                            }
+                            ?>
+                        </div>
+                        <div class="row">
+                            <hr style="width:100%; background-color: #002e5b; height: 2px; border: none;">
+                        </div>
+                        <div class="text-right">
+                            <a href="#">Điều khoản sử dụng</a>
+                            <a href="#" style="margin-left:20px">Chính sách bảo mật</a>
+                        </div>
+                    </div>
+                    <!-- <div class="col-xl-3 col-lg-3 col-md-4  col-sm-6">
+                        <div class="single-footer-caption mt-60">
+                            <div class="footer-tittle">
+                                <h4>Newsletter</h4>
+                                <p>Heaven fruitful doesn't over les idays appear creeping</p> -->
+                    <!-- Form -->
+                    <!-- <div class="footer-form" >
+                                    <div id="mc_embed_signup">
+                                        <form target="_blank" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01"
+                                        method="get" class="subscribe_form relative mail_part">
+                                            <input type="email" name="email" id="newsletter-form-email" placeholder="Email Address"
+                                            class="placeholder hide-on-focus" onfocus="this.placeholder = ''"
+                                            onblur="this.placeholder = ' Email Address '">
+                                            <div class="form-icon">
+                                            <button type="submit" name="submit" id="newsletter-submit"
+                                            class="email_icon newsletter-submit button-contactForm"><img src="assets/img/logo/form-iocn.png" alt=""></button>
+                                            </div>
+                                            <div class="mt-10 info"></div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div> -->
+                    <!-- <div class="col-xl-3 col-lg-3 col-md-5 col-sm-6">
+                        <div class="single-footer-caption mb-50 mt-60">
+                            <div class="footer-tittle">
+                                <h4>Instagram Feed</h4>
+                            </div>
+                            <div class="instagram-gellay">
+                                <ul class="insta-feed">
+                                    <li><a href="#"><img src="assets/img/post/instra1.jpg" alt=""></a></li>
+                                    <li><a href="#"><img src="assets/img/post/instra2.jpg" alt=""></a></li>
+                                    <li><a href="#"><img src="assets/img/post/instra3.jpg" alt=""></a></li>
+                                    <li><a href="#"><img src="assets/img/post/instra4.jpg" alt=""></a></li>
+                                    <li><a href="#"><img src="assets/img/post/instra5.jpg" alt=""></a></li>
+                                    <li><a href="#"><img src="assets/img/post/instra6.jpg" alt=""></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div> -->
+                </div>
+            </div>
+        </div>
+        <!-- footer-bottom aera -->
+        <!-- <div class="footer-bottom-area">
+            <div class="container">
+                <div class="footer-border">
+                    <div class="row d-flex align-items-center justify-content-between">
+                        <div class="col-lg-6">
+                            <div class="footer-copy-right">
+                                <p>
+                                    &copy;<script>
+                                        document.write(new Date().getFullYear());
+                                    </script> Tất cả bản quyền thuộc về QHA</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="footer-menu f-right">
+                                <ul>
+                                    <li><a href="#">Terms of use</a></li>
+                                    <li><a href="#">Privacy Policy</a></li>
+                                    <li><a href="#">Contact</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> -->
+        <div class="footer-bottom text-center">
+            <div class='copyright-text'>
+                <i class="fa-regular fa-copyright"></i>
+                <?php
+                    // Lấy năm hiện tại
+                    $currentYear = date("Y");
+
+                    // Hiển thị năm
+                    echo $currentYear." <span> Tất cả bản quyền thuộc về QHA</span>";
+                ?>
+            </div>
+        </div>
+        <!-- Footer End-->
+    </footer>
+    <!-- JS here -->
+
+    <!-- All JS Custom Plugins Link Here here -->
+    <script src="./assets/js/vendor/modernizr-3.5.0.min.js"></script>
+    <!-- Jquery, Popper, Bootstrap -->
+    <script src="./assets/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="./assets/js/popper.min.js"></script>
+    <script src="./assets/js/bootstrap.min.js"></script>
+    <!-- Jquery Mobile Menu -->
+    <script src="./assets/js/jquery.slicknav.min.js"></script>
+
+    <!-- Jquery Slick , Owl-Carousel Plugins -->
+    <script src="./assets/js/owl.carousel.min.js"></script>
+    <script src="./assets/js/slick.min.js"></script>
+    <!-- Date Picker -->
+    <script src="./assets/js/gijgo.min.js"></script>
+    <!-- One Page, Animated-HeadLin -->
+    <script src="./assets/js/wow.min.js"></script>
+    <script src="./assets/js/animated.headline.js"></script>
+    <script src="./assets/js/jquery.magnific-popup.js"></script>
+
+    <!-- Breaking New Pluging -->
+    <script src="./assets/js/jquery.ticker.js"></script>
+    <script src="./assets/js/site.js"></script>
+
+    <!-- Scrollup, nice-select, sticky -->
+    <script src="./assets/js/jquery.scrollUp.min.js"></script>
+    <script src="./assets/js/jquery.nice-select.min.js"></script>
+    <script src="./assets/js/jquery.sticky.js"></script>
+
+    <!-- contact js -->
+    <script src="./assets/js/contact.js"></script>
+    <script src="./assets/js/jquery.form.js"></script>
+    <script src="./assets/js/jquery.validate.min.js"></script>
+    <script src="./assets/js/mail-script.js"></script>
+    <script src="./assets/js/jquery.ajaxchimp.min.js"></script>
+
+    <!-- Jquery Plugins, main Jquery -->
+    <script src="./assets/js/plugins.js"></script>
+    <script src="./assets/js/main.js"></script>
+
+</body>
+
+</html>
