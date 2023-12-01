2735c198bfc9748a2873b51f936ef06a50b6f066
hieuu123 <140571471+hieuu123@users.noreply.github.com>
2023-12-01 23:04:56 +0700

    Upload Terms & Policy file

---
+++ policy.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>News HTML-5 Template </title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
+    <!-- CSS here -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/ticker-style.css">
+    <link rel="stylesheet" href="assets/css/flaticon.css">
+    <link rel="stylesheet" href="assets/css/slicknav.css">
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/css/themify-icons.css">
+    <link rel="stylesheet" href="assets/css/slick.css">
+    <link rel="stylesheet" href="assets/css/nice-select.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/hqn.css">
+
+    <!-- Fontawesome -->
+    <script src="https://kit.fontawesome.com/d3b4b6d594.js" crossorigin="anonymous"></script>
+
+    <style>
+        .policy-content a {
+            color: blue;
+        }
+
+        .policy-content a:hover {
+            color: blue;
+            text-decoration: underline;
+        }
+
+        .policy-content {
+            margin: 40px 30px 50px 40px;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+    include_once("header.php");
+    ?>
+    <div class="policy-content">
+        <h1>Chính sách bảo mật</h1>
+        <p>Chào mừng bạn đến với trang web tin tức QHA của chúng tôi. Chúng tôi rất trân trọng sự tin tưởng 
+        và quan tâm của bạn đối với việc bảo vệ thông tin cá nhân của mình. Chính sách bảo mật này mô tả 
+        cách chúng tôi thu thập, sử dụng và bảo vệ thông tin cá nhân của bạn khi bạn truy cập trang web của 
+        chúng tôi.</p>
+        <h2>1. Thu Thập Thông Tin Cá Nhân</h2>
+        <p>Chúng tôi có thể thu thập thông tin cá nhân từ bạn thông qua các hình thức đăng ký, đăng nhập, 
+        hoặc khi bạn tương tác với các tính năng của trang web chúng tôi. Thông tin có thể bao gồm tên, 
+        địa chỉ email, địa chỉ IP, và thông tin đăng nhập</p>
+        <h2>2. Sử Dụng Thông Tin</h2>
+        <p>Chúng tôi sử dụng thông tin cá nhân của bạn để cung cấp và cải thiện trải nghiệm của bạn trên trang 
+        web. Chúng tôi cũng có thể sử dụng thông tin để gửi thông báo quan trọng, cập nhật về sản phẩm và dịch 
+        vụ, và mục đích quảng cáo.</p>
+        <h2>3. Bảo Vệ Thông Tin</h2>
+        <p>Chúng tôi cam kết bảo vệ thông tin cá nhân của bạn. Chúng tôi triển khai các biện pháp bảo mật vật 
+        lý, điện tử và quản lý để ngăn chặn việc truy cập trái phép, tiếp cận, sử dụng, tiết lộ, thay đổi, 
+        hoặc hủy thông tin cá nhân.</p>
+        <h2>4. Chia Sẻ Thông Tin</h2>
+        <p>Chúng tôi không chia sẻ thông tin cá nhân của bạn với bên thứ ba trừ khi có sự đồng ý của bạn hoặc 
+        khi chúng tôi bắt buộc phải làm như vậy theo luật pháp.</p>
+        <h2>5. Cookie và Công Cụ Theo Dõi</h2>
+        <p>Chúng tôi sử dụng cookie và công cụ theo dõi để theo dõi hoạt động của người dùng trên trang web và 
+        cung cấp nội dung cá nhân hóa. Bạn có thể điều chỉnh cài đặt cookie trong trình duyệt của mình.</p>
+        <h2>6. Liên Kết Đến Trang Web Bên Ngoài</h2>
+        <p>Trang web của chúng tôi có thể chứa liên kết đến các trang web bên ngoài không thuộc sở hữu hoặc 
+        kiểm soát của chúng tôi. Chúng tôi không chịu trách nhiệm về nội dung hoặc chính sách bảo mật của những 
+        trang web này.</p>
+        <h2>7. Sự Thay Đổi vào Chính Sách Bảo Mật</h2>
+        <p>Chúng tôi có thể điều chỉnh chính sách bảo mật này từ thời gian này sang thời gian khác để phản ánh 
+        các thay đổi trong thực tế của chúng tôi hoặc các yêu cầu pháp lý. Bất kỳ thay đổi nào sẽ được thông 
+        báo trước.</p>
+        <h2>Liên Hệ</h2>
+        <p>Nếu bạn có bất kỳ câu hỏi hoặc lo ngại nào liên quan đến chính sách bảo mật của chúng tôi, hãy liên hệ với chúng tôi qua địa chỉ email 
+        <a href="mailto:contactqha@gmail.com" target="_blank">contactqha@gmail.com</a></p>
+        <p>Cảm ơn bạn đã đọc chính sách bảo mật của chúng tôi và đã tin tưởng sử dụng trang web của 
+        chúng tôi.</p>
+    </div>
+    <?php
+    include_once("footer.php");
+    ?>
---
+++ terms.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>News HTML-5 Template </title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
+    <!-- CSS here -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/ticker-style.css">
+    <link rel="stylesheet" href="assets/css/flaticon.css">
+    <link rel="stylesheet" href="assets/css/slicknav.css">
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/css/themify-icons.css">
+    <link rel="stylesheet" href="assets/css/slick.css">
+    <link rel="stylesheet" href="assets/css/nice-select.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/hqn.css">
+
+    <!-- Fontawesome -->
+    <script src="https://kit.fontawesome.com/d3b4b6d594.js" crossorigin="anonymous"></script>
+
+    <style>
+        .terms-content a {
+            color: blue;
+        }
+
+        .terms-content a:hover {
+            color: blue;
+            text-decoration: underline;
+        }
+
+        .terms-content {
+            margin: 40px 30px 50px 40px;
+        }
+
+        #first-h2 {
+            margin-top: 20px;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+    include_once("header.php");
+    ?>
+    <div class="terms-content">
+
+        <h1>Điều khoản sử dụng</h1>
+        <i>Khi truy cập vào các trang web tin tức QHA, người dùng đồng ý các điều khoản sau:</i>
+        <h2 id="first-h2">1. Chính sách bảo mật</h2>
+        <p>Chính sách bảo mật này giải thích cách chúng tôi sử dụng và bảo mật dữ liệu cá nhân với các dịch vụ
+            được cung cấp trên QHA. Dữ liệu cá nhân là thông tin dưới dạng ký hiệu, chữ viết, chữ số, hình ảnh,
+            âm thanh hoặc tương tự trên môi trường điện tử gắn liền với một con người cụ thể hoặc giúp xác định một
+            con người cụ thể.</p>
+        <h3>1.1. Thu thập dữ liệu</h3>
+        <p>Dữ liệu cá nhân được thu thập là những thông tin bạn đã công khai hoặc chủ động cung cấp
+            cho chúng tôi.</p>
+        <p>Việc thu thập dữ liệu đảm bảo tuân thủ quy định của pháp luật hiện hành.</p>
+        <h3>1.2. Sử dụng dữ liệu</h3>
+        <p>Chúng tôi có thể sử dụng dữ liệu được phép thu thập với mục đích tối ưu hóa trải nghiệm người dùng,
+            phân phối quảng cáo hiển thị và gửi email giới thiệu thông tin về sự kiện của QHA.</p>
+        <h3>1.3. Bảo mật dữ liệu</h3>
+        <p>Mọi thông tin cá nhân được thu thập sẽ được bảo mật bằng các biện pháp cần thiết và sẽ không được
+            cung cấp cho bất kỳ bên thứ ba nào, ngoại trừ các trường hợp theo quy định của pháp luật.</p>
+        <h3>1.4. Cập nhật/sửa chữa và xóa dữ liệu</h3>
+        <p>Bạn có quyền cập nhật/sửa chữa và xóa dữ liệu cá nhân của bạn; gửi yêu cầu xử lý dữ liệu cá nhân
+            của bạn, bằng cách liên hệ đến địa chỉ email:<a href="mailto:contactqha@gmail.com" target="_blank">
+                contactqha@gmail.com</a></p>
+        <h2>2. Bản quyền và thương hiệu</h2>
+        <p>Tất cả nội dung, hình ảnh hiển thị trên trang web tin tức QHA đều thuộc quyền sở hữu của QHA và
+            được bảo hộ theo quy định của Luật Sở hữu trí tuệ và các văn bản liên quan khác.</p>
+        <p>Mọi hành vi sao chép, phân phối, xuất bản… bất kỳ nội dung, hình ảnh nào thuộc QHA vì mục đích
+            thương mại dưới mọi hình thức mà không được sự chấp thuận trước bằng văn bản của QHA là xâm phạm quyền
+            sở hữu của QHA. QHA có quyền yêu cầu người sử dụng chấm dứt việc sử dụng và bồi thường thiệt hại
+            (nếu có).</p>
+        <h2>3. Miễn trừ trách nhiệm</h2>
+        <p>Các trang web QHA có thể liên kết tới nhiều website khác. Chúng tôi không chịu trách
+            nhiệm về nội dung thông tin cũng như mọi vấn đề xảy ra liên quan đến việc sử dụng các địa chỉ
+            liên kết này.</p>
+        <p>Người dùng có thể thấy tài liệu quảng cáo của bên thứ ba trên QHA. Mỗi nhà quảng cáo tự chịu trách
+            nhiệm về nội dung tài liệu quảng cáo của mình. Chúng tôi không chịu trách nhiệm về nội dung của tài
+            liệu quảng cáo, bao gồm nhưng không giới hạn ở bất kỳ lỗi, thiếu sót nào.</p>
+        <h2>4. Nội dung người dùng cung cấp</h2>
+        <p>Khi bạn gửi bài viết, bình luận hoặc bất kỳ nội dung nào cho chúng tôi đồng nghĩa bạn cho phép chúng
+            tôi sử dụng chúng vô điều kiện, miễn phí bản quyền, có thể chuyển nhượng hoàn toàn, vĩnh viễn trên
+            toàn thế giới để sử dụng, xuất bản và/hoặc truyền tải; đồng thời cho phép các bên thứ ba sử dụng,
+            xuất bản và/hoặc truyền tải nội dung của bạn ở bất kỳ định dạng nào và trên bất kỳ nền tảng nào,
+            hiện đã biết hoặc sau này được phát minh.</p>
+        <p>Chúng tôi hoặc các bên thứ ba được ủy quyền có thể chỉnh sửa hoặc từ chối xuất bản nội dung của
+            bạn theo quyết định riêng của chúng tôi hoặc của họ. Chúng tôi có thể xóa nội dung của bạn bất kỳ
+            lúc nào. Chúng tôi không chịu trách nhiệm pháp lý đối với bất kỳ nội dung nào do người dùng gửi và
+            được chúng tôi hoặc bên thứ ba được ủy quyền xuất bản.</p>
+        <h2>5. Thay đổi các điều khoản và điều kiện sử dụng này</h2>
+        <p>QHA có quyền sửa đổi, bổ sung, thay thế và/hoặc cắt bỏ một phần các quy định này, đồng thời
+            có quyền thay đổi và/hoặc chấm dứt các nội dung, tính năng của một phần hay toàn bộ của các trang web
+            tin tức QHA mà không có nghĩa vụ phải báo trước cho người sử dụng. Vui lòng theo dõi thường xuyên
+            vì mọi sửa đổi sẽ có hiệu lực sau khi được đăng tải trên các trang web và ứng dụng của chúng tôi.</p>
+        <p>Việc tiếp tục truy cập các trang web tin tức QHA cho thấy bạn tiếp tục chấp nhận các điều khoản này.</p>
+        <p>Mọi ý kiến đóng góp xin gửi về địa chỉ email: <a href="mailto:contactqha@gmail.com" target="_blank">
+                contactqha@gmail.com</a></p>
+    </div>
+    <?php
+    include_once("footer.php");
+    ?>
+</body>
